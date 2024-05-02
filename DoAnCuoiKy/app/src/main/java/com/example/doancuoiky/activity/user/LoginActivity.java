883af00bd0d8921301b080e5e6d8d4ba910ffa5f
NXT1008 <nguyenxuanthe10082003@gmail.com>
2024-05-02 14:44:45 +0700

    Login

--- DoAnCuoiKy/app/src/main/java/com/example/doancuoiky/activity/user/LoginActivity.java
+++ DoAnCuoiKy/app/src/main/java/com/example/doancuoiky/activity/user/LoginActivity.java
@@ -1,6 +1,10 @@
 package com.example.doancuoiky.activity.user;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,9 +12,26 @@ import com.example.doancuoiky.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText edtUsername, edtPassword;
+    Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mapping();
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                intent.putExtra("username", edtUsername.getText().toString());
+                intent.putExtra("password", edtPassword.getText().toString());
+            }
+        });
+    }
+
+    private void mapping() {
+        edtUsername = findViewById(R.id.edtUserName);
+        edtPassword = findViewById(R.id.edtPassworrd);
+        btnLogin = findViewById(R.id.btnLogin);
     }
 }
